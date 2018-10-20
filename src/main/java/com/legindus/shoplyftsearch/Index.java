f907dba9ba4462b0aab57ff96acdd5ad1859234f
Sanjay Yepuri <sanjay@Sanjays-MacBook-Pro.local>
2018-10-20 18:55:01 -0500

    Added search implementation

--- src/main/java/com/legindus/shoplyftsearch/Index.java
+++ src/main/java/com/legindus/shoplyftsearch/Index.java
@@ -4,13 +4,15 @@ import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.*;
 import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.queryparser.complexPhrase.ComplexPhraseQueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.*;
 import org.apache.lucene.index.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -43,8 +45,30 @@ public class Index {
         indexSearcher = new IndexSearcher(reader);
     }
 
-    public List<String> search(String query) {
-        return null;
+    public List<String> search(String query) throws IOException {
+        QueryParser parser = new QueryParser("keywords", analyser);
+
+        Query q = parser.parse(query);
+        System.out.println(q.toString());
+
+        TopDocs hits;
+        ScoreDoc[] docs;
+        try {
+            hits = indexSearcher.search(q, 10);
+            docs = hits.scoreDocs;
+        } catch (IOException e) {
+            System.err.println("Search Error in Index::search(): " + e.getMessage());
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        for(ScoreDoc doc: docs) {
+            int docId = doc.doc;
+            Document d = indexSearcher.doc(docId);
+            list.add(d.get("category"));
+        }
+
+        System.out.println(list);
+        return list;
     }
-    
+
 }
