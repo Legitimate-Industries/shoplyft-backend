e59e18322dabc69d60cc8b76db86dc39961093cd
David Wang <David-Wang@Outlook.com>
2018-10-20 16:57:02 -0500

    Testing firebase

--- src/main/java/com/legindus/shoplyft/Main.java
+++ src/main/java/com/legindus/shoplyft/Main.java
@@ -1,7 +1,32 @@
 package com.legindus.shoplyft;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
 public class Main {
+    private Logger LOG = LoggerFactory.getLogger(Main.class);
+    private FirebaseOptions options;
+
+    public Main() {
+        try {
+            options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.getApplicationDefault())
+                    .setDatabaseUrl("legindus-hacktx-2018.firebaseapp.com")
+                    .build();
+
+            FirebaseApp.initializeApp();
+        } catch (IOException e) {
+            LOG.error("Error connecting to firebase: ", e);
+        }
+
+    }
+
     public static void main(String[] args) {
-        
+        new Main();
     }
 }
