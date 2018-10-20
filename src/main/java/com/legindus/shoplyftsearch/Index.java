dfb733f432d27fd996af2f5ea1e0265da816e7e0
Sanjay Yepuri <sanjay@Sanjays-MacBook-Pro.local>
2018-10-20 16:48:51 -0500

    Intial Search Classes

---
+++ src/main/java/com/legindus/shoplyftsearch/Index.java
@@ -0,0 +1,8 @@
+package com.legindus.shoplyftsearch;
+
+import java.util.List;
+
+public interface Index {
+    List<String> search(String query);
+}
+
---
+++ src/main/java/com/legindus/shoplyftsearch/InvertedIndex.java
@@ -0,0 +1,16 @@
+package com.legindus.shoplyftsearch;
+
+import java.util.List;
+
+public class InvertedIndex implements Index{
+
+    public InvertedIndex(List<Document> docs) {
+        
+    }
+
+    @Override
+    public List<String> search(String query) {
+        return null;
+    }
+
+}
