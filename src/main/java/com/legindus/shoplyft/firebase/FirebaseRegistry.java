ff5d8fbbf41ced839984ef66ed4b40f78e773508
David Wang <David-Wang@Outlook.com>
2018-10-21 03:01:44 -0500

    FirebaseRegistry Javadoc

--- src/main/java/com/legindus/shoplyft/firebase/FirebaseRegistry.java
+++ src/main/java/com/legindus/shoplyft/firebase/FirebaseRegistry.java
@@ -26,6 +26,17 @@ public class FirebaseRegistry {
         firebase = new Firebase();
     }
 
+    /**
+     * Loads database and retrieves count for number of categories. Then retrieves all
+     * {@link com.legindus.shoplyft.firebase.models.CategoryDocument} and if the count matches,
+     * returns for further use by search.
+     *
+     * @return Immutable list of {@link com.legindus.shoplyft.firebase.models.CategoryDocument}
+     * @throws ExecutionException
+     *         If an attempt is made to retrieve result of task that aborted
+     * @throws InterruptedException
+     *         If future is interrupted while running
+     */
     @Nullable
     public List<CategoryDocument> getDocs() throws ExecutionException, InterruptedException {
         List<CategoryDocument> docs = new ArrayList<>();
@@ -62,6 +73,20 @@ public class FirebaseRegistry {
         return listFuture.get();
     }
 
+    /**
+     * Grabs a specified User Query with the given unique id.
+     * User the returned {@link com.legindus.shoplyft.firebase.models.Query} to
+     * provide further details on user status and chat history.
+     *
+     * @param  id
+     *         The User Query id to retrieve
+     * @return {@link com.legindus.shoplyft.firebase.models.Query}
+     *         User Query containing status and chat history
+     * @throws ExecutionException
+     *         If an attempt is made to retrieve result of task that aborted
+     * @throws InterruptedException
+     *         If future is interrupted while running
+     */
     @Nullable
     public Query getQuery(String id) throws ExecutionException, InterruptedException {
         CompletableFuture<Query> future = new CompletableFuture<>();
@@ -76,10 +101,30 @@ public class FirebaseRegistry {
         return future.get();
     }
 
+    /**
+     * Adds a User Query to the database given the {@link com.legindus.shoplyft.firebase.models.Query}
+     * with the specified unique id.
+     *
+     * @param query
+     *        The User Query to add
+     * @param id
+     *        The specified id of the User Query
+     */
     public void newQuery(Query query, String id) {
         firebase.getFDatabase().getReference("queries").child(id).setValueAsync(query);
     }
 
+    /**
+     * Updates the User Query with the given unique id with the provided
+     * {@link com.legindus.shoplyft.firebase.models.Query} if it exists.
+     *
+     * Does nothing if the User Query does not exist in the database!
+     *
+     * @param newQuery
+     *        The updated User Query to replace the old one
+     * @param id
+     *        The id of the User Query to be updated
+     */
     public void updateQuery(Query newQuery, String id) {
         DatabaseReference r = firebase.getFDatabase().getReference("queries");
         r.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -96,6 +141,12 @@ public class FirebaseRegistry {
         });
     }
 
+    /**
+     * Removes User Query from database given specified unique id.
+     *
+     * @param id
+     *        The specified User Query id to remove
+     */
     public void removeQuery(String id) {
         firebase.getFDatabase().getReference("queries").child(id).removeValueAsync();
     }
