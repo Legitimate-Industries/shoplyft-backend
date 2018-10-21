894b2de3a008b17d089940f332872bcebddf7692
David Wang <David-Wang@Outlook.com>
2018-10-21 01:09:45 -0500

    Gutted ChildEventListener

--- src/main/java/com/legindus/shoplyft/firebase/FirebaseRegistry.java
+++ src/main/java/com/legindus/shoplyft/firebase/FirebaseRegistry.java
@@ -1,10 +1,11 @@
 package com.legindus.shoplyft.firebase;
 
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 import com.legindus.shoplyft.firebase.database.Firebase;
+import com.legindus.shoplyft.firebase.listeners.ChildAddedListener;
 import com.legindus.shoplyft.firebase.models.CategoryDocument;
 import com.legindus.shoplyft.firebase.models.Query;
 import org.slf4j.Logger;
@@ -15,7 +16,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class FirebaseRegistry {
@@ -28,69 +28,6 @@ public class FirebaseRegistry {
 
     @Nullable
     public List<CategoryDocument> getDocs() throws ExecutionException, InterruptedException {
-        Future<List<CategoryDocument>> future = loadDocuments();
-        List<CategoryDocument> docs = new ArrayList<>();
-
-        try {
-            docs = future.get();
-        } catch (Exception e) {
-            LOG.error("Error grabbing documents: ", e);
-        }
-
-        return docs;
-    }
-
-    @Nullable
-    public Query getQuery(String id) throws ExecutionException, InterruptedException {
-        CompletableFuture<Query> future = new CompletableFuture<>();
-        firebase.getFDatabase().getReference("queries").addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot snapshot, String previousChildName) {
-                if (snapshot.getKey().equals(id))
-                    future.complete(snapshot.getValue(Query.class));
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot snapshot, String previousChildName) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot snapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot snapshot, String previousChildName) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-
-            }
-        });
-
-        return future.get();
-    }
-
-    public void updateQuery(int status, List<String> chats, String id) {
-        updateQuery(new Query(status, chats), id);
-    }
-
-    public void updateQuery(Query newQuery, String id) {
-        firebase.getFDatabase().getReference("queries").child(id).setValueAsync(newQuery);
-    }
-
-    public void removeQuery(String id) {
-        firebase.getFDatabase().getReference("queries").child(id).removeValueAsync();
-    }
-
-    public Firebase getFirebase() {
-        return firebase;
-    }
-
-    private Future<List<CategoryDocument>> loadDocuments() throws ExecutionException, InterruptedException {
         List<CategoryDocument> docs = new ArrayList<>();
 
         CompletableFuture<Integer> count = new CompletableFuture<>();
@@ -103,14 +40,14 @@ public class FirebaseRegistry {
 
             @Override
             public void onCancelled(DatabaseError error) {
-
+                LOG.error("Firebase grab cancelled: ", error);
             }
         });
 
         int ct = count.get();
         CompletableFuture<List<CategoryDocument>> listFuture = new CompletableFuture<>();
         AtomicInteger numReceived = new AtomicInteger(0);
-        firebase.getFDatabase().getReference("categories").orderByChild("name").addChildEventListener(new ChildEventListener() {
+        firebase.getFDatabase().getReference("categories").addChildEventListener(new ChildAddedListener() {
             @Override
             public void onChildAdded(DataSnapshot snapshot, String previousChildName) {
                 CategoryDocument document = snapshot.getValue(CategoryDocument.class);
@@ -120,28 +57,50 @@ public class FirebaseRegistry {
                 if (res == ct)
                     listFuture.complete(docs);
             }
+        });
 
+        return listFuture.get();
+    }
+
+    @Nullable
+    public Query getQuery(String id) throws ExecutionException, InterruptedException {
+        CompletableFuture<Query> future = new CompletableFuture<>();
+        firebase.getFDatabase().getReference("queries").addChildEventListener(new ChildAddedListener() {
             @Override
-            public void onChildChanged(DataSnapshot snapshot, String previousChildName) {
-
+            public void onChildAdded(DataSnapshot snapshot, String previousChildName) {
+                if (snapshot.getKey().equals(id))
+                    future.complete(snapshot.getValue(Query.class));
             }
+        });
 
+        return future.get();
+    }
+
+    public void newQuery(Query query, String id) {
+        firebase.getFDatabase().getReference("queries").child(id).setValueAsync(query);
+    }
+
+    public void updateQuery(Query newQuery, String id) {
+        DatabaseReference r = firebase.getFDatabase().getReference("queries");
+        r.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onChildRemoved(DataSnapshot snapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot snapshot, String previousChildName) {
-
+            public void onDataChange(DataSnapshot snapshot) {
+                if (snapshot.hasChild(id))
+                    r.setValueAsync(newQuery);
             }
 
             @Override
             public void onCancelled(DatabaseError error) {
-
+                LOG.error("Firebase grab cancelled: ", error);
             }
         });
+    }
 
-        return listFuture;
+    public void removeQuery(String id) {
+        firebase.getFDatabase().getReference("queries").child(id).removeValueAsync();
+    }
+
+    public Firebase getFirebase() {
+        return firebase;
     }
 }
