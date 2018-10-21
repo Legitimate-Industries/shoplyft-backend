4a90233d8981bed55aeef961953072b47e9cea1b
David Wang <David-Wang@Outlook.com>
2018-10-21 05:04:35 -0500

    Update Query
    Added employee name field

--- src/main/java/com/legindus/shoplyft/firebase/models/Query.java
+++ src/main/java/com/legindus/shoplyft/firebase/models/Query.java
@@ -11,16 +11,18 @@ public class Query extends ModelBuilder {
     public long timestamp;
     public int status;
     public String first;
+    public String employeeName;
     public List<String> chat;
 
     public Query() {
     }
 
-    public Query(long id, long timestamp, int status, String first, List<String> chat) {
+    public Query(long id, long timestamp, int status, String first, String employeeName, List<String> chat) {
         this.id = id;
         this.timestamp = timestamp;
         this.status = status;
         this.first = first;
+        this.employeeName = employeeName;
         this.chat = chat;
     }
 
@@ -29,11 +31,12 @@ public class Query extends ModelBuilder {
         private long timestamp;
         private int status;
         private String first;
+        private String employeeName;
         private List<String> chat = new ArrayList<>();
 
         @Override
         public Query build() {
-            return new Query(id, timestamp, status, first, chat);
+            return new Query(id, timestamp, status, first, employeeName, chat);
         }
 
         public final Builder setId(long id) {
@@ -53,6 +56,11 @@ public class Query extends ModelBuilder {
 
         public final Builder setFirst(String first) {
             this.first = first;
+            return this;
+        }
+
+        public final Builder setEmployeeName(String employeeName) {
+            this.employeeName = employeeName;
             return this;
         }
 
---
+++ src/main/java/com/legindus/shoplyftqueue/entities/Employee.java
@@ -0,0 +1,11 @@
+package com.legindus.shoplyftqueue.entities;
+
+public class Employee {
+    public int age;
+    public String name;
+
+    public Employee(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+}
