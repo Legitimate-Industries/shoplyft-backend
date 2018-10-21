706c7471dffd786d0dba181bde4494a941ae16d2
David Wang <David-Wang@Outlook.com>
2018-10-21 04:18:10 -0500

    Query changes
    Added timestamp, id, and first message as child fields

--- src/main/java/com/legindus/shoplyft/Main.java
+++ src/main/java/com/legindus/shoplyft/Main.java
@@ -3,11 +3,10 @@ package com.legindus.shoplyft;
 import com.legindus.shoplyft.firebase.FirebaseRegistry;
 
 public class Main {
-    private static FirebaseRegistry registry;
+    public static FirebaseRegistry registry;
 
     public final static Object lock = new Object();
     public static volatile boolean die = false;
-
 
     public static void main(String[] args) throws InterruptedException {
         registry = new FirebaseRegistry();
--- src/main/java/com/legindus/shoplyft/firebase/models/Query.java
+++ src/main/java/com/legindus/shoplyft/firebase/models/Query.java
@@ -7,28 +7,52 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Query extends ModelBuilder {
+    public long id;
+    public long timestamp;
     public int status;
+    public String first;
     public List<String> chat;
 
     public Query() {
     }
 
-    public Query(int status, List<String> chat) {
+    public Query(long id, long timestamp, int status, String first, List<String> chat) {
+        this.id = id;
+        this.timestamp = timestamp;
         this.status = status;
+        this.first = first;
         this.chat = chat;
     }
 
     public static class Builder extends ModelBuilder.Builder<Builder, Query> {
+        private long id;
+        private long timestamp;
         private int status;
+        private String first;
         private List<String> chat = new ArrayList<>();
 
         @Override
         public Query build() {
-            return new Query(status, chat);
+            return new Query(id, timestamp, status, first, chat);
+        }
+
+        public final Builder setId(long id) {
+            this.id = id;
+            return this;
+        }
+
+        public final Builder setTimestamp(long timestamp) {
+            this.timestamp = timestamp;
+            return this;
         }
 
         public final Builder setStatus(int status) {
             this.status = status;
+            return this;
+        }
+
+        public final Builder setFirst(String first) {
+            this.first = first;
             return this;
         }
 
